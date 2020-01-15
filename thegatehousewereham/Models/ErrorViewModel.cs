54fd6cea187b940de92ccfb4c4162a649bbb7185
Andrew Douglas <potterboy67@outlook.com>
2020-01-15 15:29:14 +0000

    Add project files.

---
+++ thegatehousewereham/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace thegatehousewereham.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
