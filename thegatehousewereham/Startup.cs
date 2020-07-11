3d650fde2c0e2d5f10b871246779456c93de96d5
Andrew Douglas <potterboy67@outlook.com>
2020-07-11 17:56:41 +0100

    Rejigged some text, trying to make some progress with the production set up but not getting there at the moment.

--- thegatehousewereham/Startup.cs
+++ thegatehousewereham/Startup.cs
@@ -49,16 +49,16 @@ namespace thegatehousewereham
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //    app.UseHsts();
+            //}
 
             // Useful intervention if required, in the middleware stack.
             /*
